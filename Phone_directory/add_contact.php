2b320ab9d60157cce5cbebd14a9da7b7ddfe9f74
talhafayyaz22 <talhafayyaz225@gmail.com>
2018-09-24 13:43:54 +0500

    add contact code addition

--- Phone_directory/add_contact.php
+++ Phone_directory/add_contact.php
@@ -1,7 +1,6 @@
-
-
 <html>
-<title="Sigin">
+<head>
+<title="Add Contact">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
@@ -9,7 +8,6 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </head>
-
 <style>
 .center-div
 {
@@ -18,7 +16,7 @@
   top: 0;
   right: 0;
   bottom: 0;
-  left: 130px;
+  left: 50px;
   width: 500px;
   height: 100px;
 
@@ -28,13 +26,13 @@
 .error {color: #FF0000;}
 </style>
 
+
 <body>
 <?php
-	session_start();
+session_start();
 
-
-$nameErr = $passErr =$Error ="";
-$name=$pass="";
+$nameErr =  $phErr =  "";
+$name=$Email=$phoneno=$id="";
 $tag="";
 if($_SERVER["REQUEST_METHOD"]=="POST"){
 
@@ -51,16 +49,33 @@ $tag=FALSE;}
 
 }}
 
-if(empty($_POST["Pass"])){
-$passErr="Password is required";
+if(empty($_POST["Email"])){
+
 $tag=FALSE;}
 else{
-$pass=trim(strip_tags($_POST["Pass"]));
+$Email=trim(strip_tags($_POST["Email"]));
 $tag=TRUE;
-if(strlen($pass)>8){
-$passErr="Password can only contain 8 ";
+
+}
+
+
+
+if(empty($_POST["phoneno"]))
+{
+$phErr="PhoneNo is required";
+$tag=FALSE;}
+else{
+$phoneno=trim(strip_tags($_POST["phoneno"]));
+$tag=TRUE;
+if(strlen($phoneno)>11&&!preg_match("/^[03][0-9]{10}$/")){
+$phErr="Password can only contain 11 digits and contain digits ";
 $tag=FALSE;}
 }
+
+$id=$_SESSION["Id"];
+
+
+
 
 $servername = "localhost";
 $username = "root";
@@ -72,57 +87,46 @@ $dbc=new mysqli($servername, $username, $password,$database);
 if ($dbc->connect_error) {
     die("Connection failed: " . $dbc->connect_error);
 } 
-else{
-echo 'connection successful';
-}
+
 
 if(Isset($_POST)){
-if(tag==TRUE){
+if($tag==TRUE){
+if(mysqli_query($dbc,"INSERT INTO contacts (name,number,Email,id) 
+VALUES ('$name','$Email',$phoneno,$id)")==TRUE){
 
 
-$result=mysqli_query($dbc,"select id from user_data where name='$name'and password1='$pass' limit 1");
 
-$row=mysqli_fetch_array($result);
-
-if(Isset($row)){
-$_SESSION["Id"]=$row['id'];
-	$_SESSION["name"]=$name;
-	
-	
-	
-header("Location: http://localhost/Phone_directory/user_panel.php");
 mysqli_close($dbc);
-	
-}
+header("Location: http://localhost/Phone_directory/user_panel.php");
 
-
-else{
-
-$Error="Name or Password is incorrect ";
-
-
-}
-}
-}
+}}}
 
 
 
 ?>
+
+
+
+
+
+
+
 <div id="main_div" style="width:90%;height:500px;border:solid;margin-left:50px;border-radius: 20px;border-color:#0090b8">
 <div class="center-div">
-<pre><h1 style="color:#0090b8">   Sign In</h1></pre>
-
-
+<img src="http://localhost/Phone_directory/add_contact_logo.png" style="width:100px;height:100px">
+<h1 style="color:#0090b8">Add Contact</h1>
 <p><span class="error">* required field</span></p>
 <form  method="post"   action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" >
  
 <pre>Name:       <input type="text" name="Name" value="<?php echo $name;?>"></input> <span class="error">* <?php echo $nameErr;?></span> </pre>
- <pre>Password:   <input type="password" name="Pass" value=""></input> </pre> <span class="error">* <?php echo $passErr;?></span>
-<pre>                          <input type="submit" value="login" class="btn btn-primary">
-<span class="error">* <?php echo $Error;?></span></pre>
+<pre>Email:      <input type="text" name="Email" value="<?php echo $pass;?>"></input> </pre> 
+ 
+<pre>PhoneNo:    <input type="text" name="phoneno" value="<?php echo $phoneno;?>"></input> <span class="error">* <?php echo $phErr;?></span> </pre>
+<pre>                          <input type="submit" value="Add" class="btn btn-primary"></pre>
 
 </form>
 </div>
 </div>
+
 </body>
 </html>
